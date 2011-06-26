a331c77e3aaefabc548a53d776a86f5d07cc50c5
ffachrial <firman.fachrial@gmail.com>
2011-06-26 20:42:16 +0700

    Add TReference Core and Data
    Signed-off-by: ffachrial <firman.fachrial@gmail.com>

---
+++ app/YTech.SIMK.WMTI.Core/Transaction/TReference.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NHibernate.Validator.Constraints;
+using SharpArch.Core;
+using SharpArch.Core.DomainModel;
+
+namespace YTech.SIMK.WMTI.Core.Transaction
+{
+    public class TReference : EntityWithTypedId<string>, IHasAssignedId<string>
+    {
+        [DomainSignature]
+        [NotNull, NotEmpty]
+        public virtual string ReferenceType { get; set; }
+        public virtual string ReferenceValue { get; set; }
+        public virtual string ReferenceStatus { get; set; }
+        public virtual string ReferenceDesc { get; set; }
+
+        public virtual string DataStatus { get; set; }
+        public virtual string CreatedBy { get; set; }
+        public virtual DateTime? CreatedDate { get; set; }
+        public virtual string ModifiedBy { get; set; }
+        public virtual DateTime? ModifiedDate { get; set; }
+        public virtual byte[] RowVersion { get; set; }
+
+        #region Implementation of IHasAssignedId<string>
+
+        public virtual void SetAssignedIdTo(string assignedId)
+        {
+            Check.Require(!string.IsNullOrEmpty(assignedId), "Assigned Id may not be null or empty");
+            Id = assignedId.Trim();
+        }
+
+        #endregion
+    }
+}
