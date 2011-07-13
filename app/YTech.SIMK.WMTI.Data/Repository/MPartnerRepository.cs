5e1b39be999a68482c1c0bb3f3ee536718e7f927
ffachrial <firman.fachrial@gmail.com>
2011-07-13 21:29:12 +0700

    Add Master Partner CRUD
    Signed-off-by: ffachrial <firman.fachrial@gmail.com>

--- app/YTech.SIMK.WMTI.Data/Repository/MPartnerRepository.cs
+++ app/YTech.SIMK.WMTI.Data/Repository/MPartnerRepository.cs
@@ -1,4 +1,7 @@
-﻿using SharpArch.Data.NHibernate;
+﻿using System.Collections.Generic;
+using NHibernate;
+using NHibernate.Criterion;
+using SharpArch.Data.NHibernate;
 using YTech.SIMK.WMTI.Core.Master;
 using YTech.SIMK.WMTI.Core.RepositoryInterfaces;
 
@@ -6,5 +9,27 @@ namespace YTech.SIMK.WMTI.Data.Repository
 {
     public class MPartnerRepository : NHibernateRepositoryWithTypedId<MPartner, string>, IMPartnerRepository
     {
+        #region Implementation of IMPartnerRepository
+
+        public IEnumerable<MPartner> GetPagedPartnerList(string orderCol, string orderBy, int pageIndex, int maxRows, ref int totalRows)
+        {
+            ICriteria criteria = Session.CreateCriteria(typeof(MPartner));
+
+            //calculate total rows
+            totalRows = Session.CreateCriteria(typeof(MPartner))
+                .SetProjection(Projections.RowCount())
+                .FutureValue<int>().Value;
+
+            //get list results
+            criteria.SetMaxResults(maxRows)
+              .SetFirstResult((pageIndex - 1) * maxRows)
+              .AddOrder(new Order(orderCol, orderBy.Equals("asc") ? true : false))
+              ;
+
+            IEnumerable<MPartner> list = criteria.List<MPartner>();
+            return list;
+        }
+
+        #endregion
     }
 }
